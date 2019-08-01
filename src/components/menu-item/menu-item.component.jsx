9b0dcf3ede2f8f0c00f2959a74b67271aca82f9f
Ademoye <opeyboy4real@gmail.com>
2019-08-01 11:42:14 -0400

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+//withRouter is a highered  order component, which is a func that takes a comp as an arg and returns you a modified comp
+import { withRouter } from 'react-router-dom';
 
 import "./menu-item.styles.scss";
 
 //objects passed from our directory component are passed into to this MenuItem functional component
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    className={`${size} menu-item`}
+    className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     {/* for hover effect */}
     <div className='background-image' style={{
@@ -19,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//note wrap your browser router around your application
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    //browserRouter tag gives the functionality of routing to our app tag, thats why its being wrapped with it
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
 
 
